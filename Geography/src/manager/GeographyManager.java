7d242ca862dbb3baeab2a60d37b7cae1c757d1e1
pavanhachholli <pavan@groupz.in>
2017-08-30 11:54:54 +0530

    Get geography details based on name added

--- Geography/src/manager/GeographyManager.java
+++ Geography/src/manager/GeographyManager.java
@@ -23,7 +23,6 @@ public class GeographyManager {
 						PropertiesUtil.getProperty("invalid_json_message"));
 
 			}
-
 			connection = connectionPooling.getConnection();
 
 			JSONObject obj = JSONObject.fromObject(req);
@@ -94,7 +93,6 @@ public class GeographyManager {
 		try {
 			if (dataObj.containsKey(GlobalVariables.LIMIT_TAG)) {
 				limit = dataObj.getInt(GlobalVariables.LIMIT_TAG);
-
 			}
 			if (dataObj.containsKey(GlobalVariables.OFFSET_TAG)) {
 				offset = dataObj.getInt(GlobalVariables.OFFSET_TAG);
@@ -129,9 +127,14 @@ public class GeographyManager {
 		int limit = -1;
 		int offset = -1;
 		int countryId = -1;
+		String countryName = null;
 		try {
-			countryId = dataObj.getInt(GlobalVariables.COUNTRYID_TAG);
-
+			if (dataObj.containsKey(GlobalVariables.COUNTRYID_TAG)) {
+				countryId = dataObj.getInt(GlobalVariables.COUNTRYID_TAG);
+			}
+			if (dataObj.containsKey("countryname")) {
+				countryName = dataObj.getString("countryname");
+			}
 			if (dataObj.containsKey(GlobalVariables.LIMIT_TAG)) {
 				limit = dataObj.getInt(GlobalVariables.LIMIT_TAG);
 
@@ -140,19 +143,37 @@ public class GeographyManager {
 				offset = dataObj.getInt(GlobalVariables.OFFSET_TAG);
 			}
 			paginationSQL = utils.paginationQry(limit, offset);
-			String getStatesSQL = "Select * from State where CountryId="
-					+ countryId + paginationSQL;
-			System.out.println("Get states Query :" + getStatesSQL);
-			JSONArray dataArray = dbo.getGeographyDetails(getStatesSQL, con,
-					GlobalVariables.STATE_LIST);
-			if (dataArray != null && dataArray.size() > 0) {
-				return utils.processSuccessWithJsonArray(serviceType,
-						functionType, dataArray);
+
+			String getStatesSQL = null;
+
+			if (countryId <= 0 && utils.isEmpty(countryName) == true) {
+				getStatesSQL = "Select s.* from state s ,country c where s.countryid=c.id and c.countryname='"
+						+ countryName + "'" + paginationSQL;
+			} else if (utils.isEmpty(countryName) == false && countryId > 0) {
+				getStatesSQL = "Select * from State where CountryId="
+						+ countryId + paginationSQL;
+			} else if (countryId > 0 && utils.isEmpty(countryName) == true) {
+				getStatesSQL = "Select S.* from State S,Country C where C.Id=S.CountryId and C.CountryName='"
+						+ countryName + "' and C.Id=" + countryId;
+			}
+			if (getStatesSQL != null) {
+				System.out.println("Get states Query : " + getStatesSQL);
+				JSONArray dataArray = dbo.getGeographyDetails(getStatesSQL,
+						con, GlobalVariables.STATE_LIST);
+				if (dataArray != null && dataArray.size() > 0) {
+					return utils.processSuccessWithJsonArray(serviceType,
+							functionType, dataArray);
+				} else {
+					return utils.processError(
+							PropertiesUtil.getProperty("states_empty_code"),
+							PropertiesUtil.getProperty("states_empty_message"));
+				}
 			} else {
 				return utils.processError(
-						PropertiesUtil.getProperty("states_empty_code"),
-						PropertiesUtil.getProperty("states_empty_message"));
+						PropertiesUtil.getProperty("general_error_code"),
+						PropertiesUtil.getProperty("general_error_message"));
 			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return utils.processError(
@@ -166,29 +187,54 @@ public class GeographyManager {
 		int limit = -1;
 		int offset = -1;
 		int stateId = -1;
+		String countryName = null;
+		String StateName = null;
 		try {
-			stateId = dataObj.getInt(GlobalVariables.STATEID_TAG);
-
+			if (dataObj.containsKey(GlobalVariables.STATEID_TAG)) {
+				stateId = dataObj.getInt(GlobalVariables.STATEID_TAG);
+			}
+			if (dataObj.containsKey("countryname")) {
+				countryName = dataObj.getString("countryname");
+			}
+			if (dataObj.containsKey("statename")) {
+				StateName = dataObj.getString("statename");
+			}
 			if (dataObj.containsKey(GlobalVariables.LIMIT_TAG)) {
 				limit = dataObj.getInt(GlobalVariables.LIMIT_TAG);
-
 			}
 			if (dataObj.containsKey(GlobalVariables.OFFSET_TAG)) {
 				offset = dataObj.getInt(GlobalVariables.OFFSET_TAG);
 			}
 			String paginationSQL = utils.paginationQry(limit, offset);
-			String getCitiesSQL = "Select * from City where StateId=" + stateId
-					+ paginationSQL;
-			System.out.println("Get Cities SQL :" + getCitiesSQL);
-			JSONArray dataArray = dbo.getGeographyDetails(getCitiesSQL, con,
-					GlobalVariables.CITY_LIST);
-			if (dataArray != null && dataArray.size() > 0) {
-				return utils.processSuccessWithJsonArray(serviceType,
-						functionType, dataArray);
+			String getCitiesSQL = null;
+			if (stateId > 0) {
+				getCitiesSQL = "Select * from City where StateId=" + stateId
+						+ paginationSQL;
+			} else if (utils.isEmpty(countryName) == true
+					&& utils.isEmpty(StateName) == true) {
+				getCitiesSQL = " Select C.* from City C,Country Ct,State S where C.StateId=S.Id and S.CountryId=Ct.Id and Ct.CountryName='"
+						+ countryName
+						+ "' and S.StateName='"
+						+ StateName
+						+ "'"
+						+ paginationSQL;
+			}
+			if (getCitiesSQL != null) {
+				System.out.println("Get Cities SQL : " + getCitiesSQL);
+				JSONArray dataArray = dbo.getGeographyDetails(getCitiesSQL,
+						con, GlobalVariables.CITY_LIST);
+				if (dataArray != null && dataArray.size() > 0) {
+					return utils.processSuccessWithJsonArray(serviceType,
+							functionType, dataArray);
+				} else {
+					return utils.processError(
+							PropertiesUtil.getProperty("city_empty_code"),
+							PropertiesUtil.getProperty("city_empty_message"));
+				}
 			} else {
 				return utils.processError(
-						PropertiesUtil.getProperty("city_empty_code"),
-						PropertiesUtil.getProperty("city_empty_message"));
+						PropertiesUtil.getProperty("general_error_code"),
+						PropertiesUtil.getProperty("general_error_message"));
 			}
 
 		} catch (Exception e) {
@@ -205,19 +251,47 @@ public class GeographyManager {
 		int limit = -1;
 		int offset = -1;
 		int cityId = -1;
+		String countryName = null;
+		String stateName = null;
+		String cityName = null;
 		try {
-			cityId = dataObj.getInt("cityid");
+
+			if (dataObj.containsKey("cityid")) {
+				cityId = dataObj.getInt("cityid");
+			}
+			if (dataObj.containsKey("countryname")) {
+				countryName = dataObj.getString("countryname");
+			}
+			if (dataObj.containsKey("statename")) {
+				stateName = dataObj.getString("statename");
+
+			}
+			if (dataObj.containsKey("cityname")) {
+				cityName = dataObj.getString("cityname");
+			}
+
 			if (dataObj.containsKey(GlobalVariables.LIMIT_TAG)) {
 				limit = dataObj.getInt(GlobalVariables.LIMIT_TAG);
 			}
 			if (dataObj.containsKey(GlobalVariables.OFFSET_TAG)) {
 				offset = dataObj.getInt(GlobalVariables.OFFSET_TAG);
 			}
-			String paginationSQL = utils.paginationQry(limit, offset);
 
-			String getAreasSQL = "Select * from Area where CityId=" + cityId
-					+ paginationSQL;
-			System.out.println("Get Areas SQL :" + getAreasSQL);
+			String paginationSQL = utils.paginationQry(limit, offset);
+			String getAreasSQL = null;
+			if (cityId > 0) {
+				getAreasSQL = "Select * from Area where CityId=" + cityId
+						+ paginationSQL;
+			} else if (utils.isEmpty(countryName) == true
+					&& utils.isEmpty(stateName)
+					&& utils.isEmpty(cityName) == true) {
+				getAreasSQL = "Select A.* from Area A,City C,State S,Country Ct where A.CityId=C.Id and C.StateId=S.Id and S.CountryId=Ct.Id and Ct.CountryName='"
+						+ countryName
+						+ "' and S.StateName='"
+						+ stateName
+						+ "' and C.CityName='" + cityName + "'" + paginationSQL;
+			}
+			System.out.println("Get Areas SQL : " + getAreasSQL);
 			JSONArray dataArray = dbo.getGeographyDetails(getAreasSQL, con,
 					GlobalVariables.AREA_LIST);
 			if (dataArray != null && dataArray.size() > 0) {
