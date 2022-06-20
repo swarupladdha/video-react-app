9bb7b31516a652d05f5160f5403fcc600c9b1cd2
jagadish <jagadish.s@infosoftjoin.in>
2022-06-20 10:04:25 +0530

    updated

--- ZoomConference/conference/src/main/java/com/zoom/mysqlaccess/ZoomMysqlOperation.java
+++
@@ -1,9 +0,0 @@
-package com.zoom.mysqlaccess;
-
-public class ZoomMysqlOperation {
-	
-	public String saveMeeting(com.zoom.dbhelper.CreateMeeting m) {
-		return null;
-	}
-	
-}
---
+++ ZoomConference/src/main/java/com/zoom/zoomvideoconference/ZoomvideoconferenceApplication.java
@@ -0,0 +1,15 @@
+package com.zoom.zoomvideoconference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomvideoconferenceApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ZoomvideoconferenceApplication.class, args);
+	}
+	
+	
+}
