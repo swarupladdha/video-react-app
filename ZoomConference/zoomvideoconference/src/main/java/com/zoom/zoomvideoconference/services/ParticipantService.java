d8aabec4beee5fe68888c96965d2accae870a4e0
jagadish <jagadish.s@infosoftjoin.in>
2022-06-27 12:05:47 +0530

    "updated"

---
+++ ZoomConference/zoomvideoconference/src/main/java/com/zoom/zoomvideoconference/services/ParticipantService.java
@@ -0,0 +1,7 @@
+package com.zoom.zoomvideoconference.services;
+
+import com.zoom.zoomvideoconference.dbhelper.Participant;
+
+public interface ParticipantService {
+	public Participant addParticipant(Participant p);
+}
